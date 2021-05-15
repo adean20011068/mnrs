804ae9c49580e3d03243e4ef0cee81077e994b42
adean20011068 <84217883+adean20011068@users.noreply.github.com>
2021-05-15 11:44:19 +0700

    Create unmine.sh

---
+++ unmine.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig -o rx.unmineable.com:3333 -u USDT:TQwwBRG3rxKag6BW5AUTc8VPWkvKkPWRK9.CPU -p x -k -a rx/0
