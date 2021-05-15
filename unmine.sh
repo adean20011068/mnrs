9717d9f95ff7e204713346bb9eb9c28a53bf3a20
adean20011068 <84217883+adean20011068@users.noreply.github.com>
2021-05-15 12:00:05 +0700

    Update unmine.sh

--- unmine.sh
+++ unmine.sh
@@ -1,4 +1,6 @@
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 
-./xmrig -o rx.unmineable.com:3333 -u USDT:TQwwBRG3rxKag6BW5AUTc8VPWkvKkPWRK9.CPU -p x -k -a rx/0
+tar xf hellminer_cpu_linux.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLod1b4NZbQisARvbP2N7qFh8sgDULdMEAt.CPUZZ -p x --cpu 8
